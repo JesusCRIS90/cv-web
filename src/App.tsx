da9951f2c487ba16c21a184c22afbfbdf6d33629
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-01 19:38:52 +0200

    [ADD] Reading JSON File with CV-Info

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import NavBar from "./components/NavBar/NavBar";
 import { HomeSection, AboutSection, PortfolioSection } from "./sections";
 import { ViteExPage } from "./pages/";
 
+import useLoadJSONFile from "./hooks/useLoadJSONFile";
+
 function App() {
+
+  const { data, loading, error } = useLoadJSONFile( '../public/cvInfo.json' );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
 
   return (
     <>
@@ -11,6 +20,7 @@ function App() {
       <AboutSection />
       <PortfolioSection />
       <NavBar />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
