96a4d059e1780be3ae4ba431c95e86fa0d5dd809
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-06 17:02:33 +0200

    [MOD] Refactoring Code Background component

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,59 +1,15 @@
-import React, { ReactElement } from 'react'
+import React, { CSSProperties, ReactElement } from 'react'
 
+import { AnchorPoint } from "../../utils/enums"
 
 import styles from "./Background.module.css"
 
 
-export enum AnchorPoint {
-    TOP_LEFT,
-    TOP_CENTER,
-    TOP_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_CENTER,
-    BOTTOM_RIGHT,
-    CENTER_LEFT,
-    CENTER_CENTER,
-    CENTER_RIGHT
-}
+function BuildCustomStyle(img: string, anchorPoint: AnchorPoint, userCustomStyle: Object): CSSProperties {
 
-function getAnchorPointToCSS(anchorPoint: AnchorPoint) {
-
-    switch (anchorPoint) {
-        case AnchorPoint.TOP_LEFT:
-            return "top left";
-
-        case AnchorPoint.TOP_CENTER:
-            return "top center";
-
-        case AnchorPoint.TOP_RIGHT:
-            return "top right";
-
-        case AnchorPoint.BOTTOM_LEFT:
-            return "bottom left";
-
-        case AnchorPoint.BOTTOM_CENTER:
-            return "bottom center";
-
-        case AnchorPoint.BOTTOM_RIGHT:
-            return "bottom right";
-
-        case AnchorPoint.CENTER_LEFT:
-            return "center left";
-
-        case AnchorPoint.CENTER_CENTER:
-            return "center";
-
-        case AnchorPoint.CENTER_RIGHT:
-            return "center right";
-    }
-
-}
-
-function BuildCustomStyle( img: string, anchorPoint: AnchorPoint, userCustomStyle: Object ){
-    
     const ImageStyle = {
         backgroundImage: `url(${img})`,
-        backgroundPosition: getAnchorPointToCSS( anchorPoint ),
+        backgroundPosition: anchorPoint,
     }
 
     return { ...ImageStyle, ...userCustomStyle };
@@ -63,25 +19,30 @@ function BuildCustomStyle( img: string, anchorPoint: AnchorPoint, userCustomStyl
 interface BackgroundProps extends React.PropsWithChildren {
     id?: string,
     className?: string,
-    style?: Object,
+    style?: CSSProperties,
     children?: ReactElement | ReactElement[],
     img?: string,
     anchorPoint?: AnchorPoint,
 }
 
-const BackGround = ({ children, id = "", className = "", style = {}, img = "",
-    anchorPoint = AnchorPoint.CENTER_CENTER }: BackgroundProps) => {
+const BackGround: React.FC<BackgroundProps> = ({
+    children,
+    id = "",
+    className = "",
+    style = {},
+    img = "",
+    anchorPoint = AnchorPoint.CENTER_CENTER
+  }) => {
 
-    const style2apply = BuildCustomStyle( img, anchorPoint, style );
-    const className2Apply = `${styles["bg"]} ${className}`;
+    const combinedStyle = BuildCustomStyle(img, anchorPoint, style);
+    const combinedClassName = `${styles["bg"]} ${className}`;
 
     return (
-        <section id={id} className={ className2Apply } style={ style2apply } >
+        <section id={id} className={combinedClassName} style={combinedStyle} >
             {children}
         </section>
     )
 }
-
 
 
 export default BackGround
---
+++ src/utils/enums.ts
@@ -0,0 +1,11 @@
+export enum AnchorPoint {
+    TOP_LEFT = "top left",
+    TOP_CENTER = "top center",
+    TOP_RIGHT = "top right",
+    BOTTOM_LEFT = "bottom left",
+    BOTTOM_CENTER = "bottom center",
+    BOTTOM_RIGHT = "bottom right",
+    CENTER_LEFT = "center left",
+    CENTER_CENTER = "center",
+    CENTER_RIGHT = "center right"
+}
