d199c5099ad83cfe286da65bc0d325583a87409e
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-19 17:08:12 +0200

    [MOD] Refactoring Code in Component SVG_IconLink

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -13,14 +13,18 @@ export interface SVG_IconProps extends React.PropsWithChildren {
     color?: string;
 }
 
-export interface SVG_IconLinkProps extends React.PropsWithChildren {
-    name: string;
+export interface SVG_IconLinkBaseProps extends React.PropsWithChildren {
     id?: string,
     className?: string,
     style?: CSSProperties,
     size?: number;
     color?: string;
     link?: string;
+}
+
+export interface SVG_IconLinkProps extends SVG_IconLinkBaseProps {
+    name: string;
+    data?: SVG_IconLinkBaseProps;
 }
 
 const SVG_Icon: React.FC<SVG_IconProps> = ({
@@ -51,16 +55,26 @@ const SVG_IconLink: React.FC<SVG_IconLinkProps> = ({
     style = {},
     size = 24,
     color = '',
-    link = ""
+    link = "",
+    data = {}
 }) => {
 
-    const combinedClassName = `${styles["svg-icon-link"]} ${className}`;
+    const {
+        id: dataId = id,
+        className: dataClassName = className,
+        style: dataStyle = style,
+        size: dataSize = size,
+        color: dataColor = color,
+        link: dataLink = link,
+    } = data;
+
+    const combinedClassName = `${styles["svg-icon-link"]} ${dataClassName}`;
 
     return (
-        <a href={link} target='_blank' rel='noreferrer'>
+        <a href={dataLink} target='_blank' rel='noreferrer'>
             <svg
-                id={id} className={combinedClassName} style={style}
-                width={size} height={size} fill={color}
+                id={dataId} className={combinedClassName} style={dataStyle}
+                width={dataSize} height={dataSize} fill={dataColor}
             >
                 <use xlinkHref={`#${name}`} />
             </svg>
