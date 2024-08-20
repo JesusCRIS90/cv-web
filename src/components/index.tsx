12e73bc5129dd2765e30cef7b6149656a17a43bd
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-20 20:03:50 +0200

    [ADD] Unified exporting components file
    An index.tsx file has been created in the level of components folder.
    This file import and export all components created, so we could import any component from the same place

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import { IconLink, IconLinkPropsBuilder, SVG_Icon, SVG_LinkPropsBuilder } from "./Icons"
+import { BackGround, Image, ImagePropsBuilder, BackgroundPropsBuilder } from "./Images"
+import { SingleText, Tittle, SingleTextPropsBuilder, TittlePropsBuilder } from "./Typography"
+
+
+export { IconLink, IconLinkPropsBuilder, SVG_Icon, SVG_LinkPropsBuilder }
+export { BackGround, Image, ImagePropsBuilder, BackgroundPropsBuilder }
+export { SingleText, Tittle, SingleTextPropsBuilder, TittlePropsBuilder }
