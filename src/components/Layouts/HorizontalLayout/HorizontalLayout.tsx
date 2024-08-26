05ba8e1f0928eda706c902baf96bd64fea78cae8
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-26 19:32:03 +0200

    [MOD] Re-Factoring Function Position Policy on Component

--- src/components/Layouts/HorizontalLayout/HorizontalLayout.tsx
+++ src/components/Layouts/HorizontalLayout/HorizontalLayout.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties, ReactElement } from 'react'
-import { POLICY_POSITION } from "../../../utils/enums"
+import { POLICY_HORIZONTAL_POSITION } from "../../../utils/enums"
 
 import styles from "./HorizontalLayout.module.css"
 
@@ -9,7 +9,7 @@ interface BaseProps extends React.PropsWithChildren {
     id?: string,
     className?: string,
     style?: CSSProperties,
-    groupPolicyPos?: POLICY_POSITION
+    groupPolicyPos?: POLICY_HORIZONTAL_POSITION
     gap?: number,
 }
 
@@ -33,7 +33,7 @@ export class HorizontalLayoutPropsBuilder {
     }
 }
 
-function BuildCustomStyle(groupPolicyPos: POLICY_POSITION, Gap: number, userCustomStyle: Object): CSSProperties {
+function BuildCustomStyle(groupPolicyPos: POLICY_HORIZONTAL_POSITION, Gap: number, userCustomStyle: Object): CSSProperties {
 
     let alignItems: string;
     let justifyContent: string;
@@ -41,47 +41,47 @@ function BuildCustomStyle(groupPolicyPos: POLICY_POSITION, Gap: number, userCust
 
     switch (groupPolicyPos) {
 
-        case POLICY_POSITION.TOP_LEFT:
+        case POLICY_HORIZONTAL_POSITION.TOP_LEFT:
             alignItems = "flex-start";
             justifyContent = "flex-start";
             break;
 
-        case POLICY_POSITION.TOP_CENTER:
+        case POLICY_HORIZONTAL_POSITION.TOP_CENTER:
             alignItems = "flex-start";
             justifyContent = "center";
             break;
 
-        case POLICY_POSITION.TOP_RIGHT:
+        case POLICY_HORIZONTAL_POSITION.TOP_RIGHT:
             alignItems = "flex-start";
             justifyContent = "flex-end";
             break;
 
-        case POLICY_POSITION.CENTER_LEFT:
+        case POLICY_HORIZONTAL_POSITION.CENTER_LEFT:
             alignItems = "center";
             justifyContent = "flex-start";
             break;
 
-        case POLICY_POSITION.CENTER_CENTER:
+        case POLICY_HORIZONTAL_POSITION.CENTER_CENTER:
             alignItems = "center";
             justifyContent = "center";
             break;
 
-        case POLICY_POSITION.CENTER_RIGHT:
+        case POLICY_HORIZONTAL_POSITION.CENTER_RIGHT:
             alignItems = "center";
             justifyContent = "flex-end";
             break;
 
-        case POLICY_POSITION.BOTTOM_LEFT:
+        case POLICY_HORIZONTAL_POSITION.BOTTOM_LEFT:
             alignItems = "flex-end";
             justifyContent = "flex-start";
             break;
 
-        case POLICY_POSITION.BOTTOM_CENTER:
+        case POLICY_HORIZONTAL_POSITION.BOTTOM_CENTER:
             alignItems = "flex-end";
             justifyContent = "center";
             break;
 
-        case POLICY_POSITION.BOTTOM_RIGHT:
+        case POLICY_HORIZONTAL_POSITION.BOTTOM_RIGHT:
             alignItems = "flex-end";
             justifyContent = "flex-end";
             break;
@@ -101,7 +101,7 @@ const HorizontalLayout: React.FC<HorizontalLayoutProps> = ({
     id = "",
     className = "",
     style = {},
-    groupPolicyPos = POLICY_POSITION.CENTER_CENTER,
+    groupPolicyPos = POLICY_HORIZONTAL_POSITION.CENTER_CENTER,
     gap = 0,
     data = {}
 }) => {
