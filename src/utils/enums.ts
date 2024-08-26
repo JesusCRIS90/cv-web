dc9bfc93676515339422802bc4f6a0f8ca7ef7ca
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-08-26 17:47:42 +0200

    [MOD] Rename PolicyPosition Enum

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -10,6 +10,18 @@ export enum AnchorPoint {
     CENTER_RIGHT = "center right"
 }
 
+export enum POLICY_POSITION {
+    TOP_LEFT = 1,
+    TOP_CENTER = 2,
+    TOP_RIGHT = 3,
+    BOTTOM_LEFT = 4,
+    BOTTOM_CENTER = 5,
+    BOTTOM_RIGHT = 6,
+    CENTER_LEFT = 7,
+    CENTER_CENTER = 8,
+    CENTER_RIGHT = 9
+}
+
 export enum TYPOGRAPHY_LEVEL {
     LEVEL_1 = "H1",
     LEVEL_2 = "H2",
