08432c5cfba8cdbc0b5ae61788f17d8ce401cf17
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-07 22:23:22 +0200

    [ADD] Data Provider on Main Root

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,16 @@ import "./index.css";
 import store from "./redux-toolkit/store/store.ts";
 import { Provider } from "react-redux";
 
+
+import { DataProvider } from "./context/DataProvider.tsx"
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <DataProvider>
+          <App />
+        </DataProvider>
       </Provider>
     </React.StrictMode>
   </>
