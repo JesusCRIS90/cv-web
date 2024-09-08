7122d3a0d2a5a1fe3f090254c9d185e1e1631b9f
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-08 22:12:28 +0200

    [MOD] Updated Home Section

--- src/sections/HomeSection/HomeSection.tsx
+++ src/sections/HomeSection/HomeSection.tsx
@@ -1,14 +1,43 @@
-import BackGround from "../../components/Background/Background";
+import { useContext } from "react"
+
+import { DataContext } from "../../context/DataProvider"
+
+import { BackGround, Image, Tittle, SingleText, IconLink } from "../../components";
 
 import styles from "./HomeSection.module.css"
 
+import '../../index.css'
+import { CenterLayout, GridLayout1D } from "../../components/Layouts";
+import { WhoIam } from '../../components/Home'
+
+
+
+
+
+
 export function HomeSection() {
 
+  const { data } = useContext(DataContext);
+  const { ProfileImage } = data.Home;
+
+  console.log(data.Home);
 
   return (
     <>
-      <BackGround id="home" img="../../public/assets/bg2.png" className={styles["home-sec"]}>
-        <div>Hello World</div>
+      <BackGround id="home" img="/assets/bg2.png" className={styles["home-sec"]}>
+
+        <CenterLayout>
+
+          <GridLayout1D distribution="40% auto">
+            
+            <Image img={ProfileImage}/>
+
+            <WhoIam Home={data.Home}/>
+
+          </GridLayout1D>
+
+        </CenterLayout>
+
       </BackGround>
     </>
   );
