a00ab8c5b1edbeb7349aad9efdc451ee652da617
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-08 22:09:27 +0200

    [ADD] Update index.ts Component to Export

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,3 +1,7 @@
 export { IconLink, IconLinkPropsBuilder } from "./IconLink/IconLink"
 export { SVG_Icon, SVG_IconPropsBuilder } from "./SVG_Icon/SVG_Icon"
 export { RoundIcon, RoundIconPropsBuilder } from "./RoundIcon/RoundIcon"
+export { ActionIcon } from "./ActionIcon/ActionIcon"
+
+export { LinkIconList } from "./LinkIconList/LinkIconList"
+export { SvgIconList } from "./SVG_IconList/SVG_IconList"
