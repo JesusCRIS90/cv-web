ad07461cf2a5752af4ee5c9f8dafc6afe73089c7
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-08 22:12:57 +0200

    [ADD] New Component ProjectCard

---
+++ src/components/Portfolio/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+import { GridLayout2D, HorizontalLayout, ItemGridLayout } from '../../Layouts';
+
+import { SingleText, Tittle } from '../../Typography';
+import { Image } from '../../Images';
+
+// import { TYPOGRAPHY_LEVEL } from '../../utils/enums';
+
+import styles from "./ProjectCard.module.css"
+import { LinkIconList, SvgIconList } from '../../Icons';
+
+
+
+// ------------------------------------------
+
+interface SVG_Icon {
+    iconName: string;
+    tooltip: string;
+}
+
+interface LinkIcon {
+    iconName: string;
+    link: string;
+}
+
+interface BaseProps extends React.PropsWithChildren {
+    id?: number;
+    tittle?: string;
+    image?: string;
+    description?: string;
+    techStack?: SVG_Icon[];
+    links?: LinkIcon[];
+}
+
+export interface ProtfolioCardProps extends BaseProps {
+    PortfolioCard?: BaseProps;
+}
+
+// ------------------------------------------
+
+
+
+
+const ProtfolioCard: React.FC<ProtfolioCardProps> = ({
+    id,
+    tittle,
+    image,
+    description,
+    techStack,
+    links,
+    PortfolioCard
+}) => {
+
+    const {
+        id: portfolioCard_Id = id,
+        tittle: portfolioCard_tittle = tittle,
+        image: portfolioCard_image = image,
+        description: portfolioCard_description = description,
+        techStack: portfolioCard_techStack = techStack,
+        links: portfolioCard_links = links,
+    } = PortfolioCard || {};
+
+
+    console.log("[ PROJECT-CARD ]", PortfolioCard);
+
+
+    return (
+
+        <GridLayout2D
+            distribution={{ x: "60% auto", y: "20% 60% auto" }}
+        >
+
+            <ItemGridLayout pos={{ x: 1, y: 1 }} size={{ x: 2, y: null }}>
+
+                <Tittle>
+                    {portfolioCard_tittle}
+                </Tittle>
+
+            </ItemGridLayout>
+
+            <Image img={portfolioCard_image} />
+
+            <SingleText>
+                {portfolioCard_description}
+            </SingleText>
+
+            <HorizontalLayout>
+                {
+                    (portfolioCard_techStack === undefined)
+                        ? <></>
+                        : <SvgIconList svgIconList={portfolioCard_techStack} />
+                }
+            </HorizontalLayout>
+
+            <HorizontalLayout>
+                {
+                    (portfolioCard_links === undefined)
+                        ? <></>
+                        : <LinkIconList linkIconList={portfolioCard_links} />
+                }
+            </HorizontalLayout>
+
+
+
+        </GridLayout2D>
+    )
+}
+
+export { ProtfolioCard }
+
+
+
+
+
+
+
+
+
+
+
