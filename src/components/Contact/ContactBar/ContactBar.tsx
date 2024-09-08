df78da02e9258aae57f21d67b94034835aa20388
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-08 22:11:17 +0200

    [MOD] Updated ContactBar Component

--- src/components/Contact/ContactBar/ContactBar.tsx
+++ src/components/Contact/ContactBar/ContactBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { IconLink } from "../../../components"
+import { ActionIcon, LinkIconList } from "../../../components"
 import { HorizontalLayout } from "../../Layouts"
 
 import styles from "./ContactBar.module.css"
 
-interface ActionIcons {
+interface ActionIcon {
     iconName: string;
     action: string;
 }
@@ -16,7 +16,7 @@ interface LinkIcon {
 }
 
 interface BaseProps extends React.PropsWithChildren {
-    actionIcons?: ActionIcons[];
+    actionIcon?: ActionIcon;
     linkIcons?: LinkIcon[];
 }
 
@@ -27,39 +27,29 @@ export interface ContactProps extends BaseProps {
 
 const ContactBar: React.FC<ContactProps> = ({
     linkIcons,
-    actionIcons,
+    actionIcon,
     contact
 }) => {
 
     const {
         linkIcons: contactLinkIcons = linkIcons || [],
-        actionIcons: contactActionIcons = actionIcons || [],
+        actionIcon: contactActionIcon = actionIcon,
     } = contact || {};
+
+    const iconActionName = ( contactActionIcon === undefined ) ? "" : contactActionIcon.iconName;
 
     // const mergedLinkIcons = contact?.linkIcons || linkIcons || [];
     // const mergedActionIcons = contact?.actionIcons || actionIcons || [];
 
 
-    // console.log("[ CONTACT-BAR ]", mergedLinkIcons);
+    console.log("[ CONTACT-BAR ]", contactActionIcon);
 
     return (
+
         <HorizontalLayout className={`${styles['contact-bar-layout']}`}>
 
-            {
-                (contactLinkIcons.length > 0 && contactLinkIcons === undefined)
-                    ? <></>
-                    : contactLinkIcons.map((icon: LinkIcon) => {
-                        const { iconName, link } = icon;
-                        return <IconLink
-                            className={`${styles['contact-bar-icon']}`}
-                            name={iconName}
-                            key={iconName}
-                            link={link}
-                            size={32}
-                        />
-                    })
-            }
-
+            <LinkIconList linkIconList={contactLinkIcons} />
+            <ActionIcon name={iconActionName}/>
 
         </HorizontalLayout>
     )
