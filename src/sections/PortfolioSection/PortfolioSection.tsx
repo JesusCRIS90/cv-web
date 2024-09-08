96bb43eb3e6a08406c3c908ce59f27dcff7a95fb
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-08 22:13:39 +0200

    [MOD] Updated Portolio Section

--- src/sections/PortfolioSection/PortfolioSection.tsx
+++ src/sections/PortfolioSection/PortfolioSection.tsx
@@ -1,9 +1,30 @@
+import { useContext } from "react";
+import { ProtfolioCard } from "../../components/Portfolio"
+
 import styles from "./PortfolioSection.module.css";
+import { DataContext } from "../../context/DataProvider";
+import { Tittle } from "../../components";
 
 export function PortfolioSection() {
+
+  const { data } = useContext(DataContext);
+  const { Portfolio } = data;
+
+  // console.log("[SECTION-PORTFOLIO]", Portfolio);
+
+
   return (
     <>
-      <section id="portfolio" className={styles["portfolio-sec"]}></section>
+      <section id="portfolio" className={styles["portfolio-sec"]}>
+
+        <Tittle>
+          Projects
+        </Tittle>
+
+        <ProtfolioCard PortfolioCard={Portfolio[1]} />
+
+      </section>
     </>
   );
 }
+
