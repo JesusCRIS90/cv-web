c7ce8b655d4f8d7abaebf3f4bad72c8e70ef8011
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-09 22:58:44 +0200

    [MOD] Implementation Components in About Section

--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -1,9 +1,34 @@
+import { useContext } from "react";
+import { DataContext } from "../../context/DataProvider";
 import styles from "./AboutSection.module.css";
+import { SkillCard } from "../../components/Sections";
 
 export function AboutSection() {
+
+  const { data } = useContext(DataContext);
+
+
+  const testSkillCard = data.About.TechStack.listData[0];
+
+  console.log("[ABOUT SECTION]", data.About, testSkillCard);
+
   return (
     <>
-      <section id="about" className={styles["about-sec"]}></section>
+      <section id="about" className={styles["about-sec"]}>
+
+        <SkillCard 
+          description={testSkillCard.skillDescription} 
+          name={testSkillCard.skillName}
+          tag={testSkillCard.tag}
+          image={testSkillCard.image}
+        />
+
+        {/* <SkillCard 
+          skillDataObj={testSkillCard}
+        /> */}
+
+
+      </section>
     </>
   );
 }
