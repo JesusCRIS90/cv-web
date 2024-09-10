a1683f090a85795115097793879dbad9305e0442
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-10 20:08:23 +0200

    [MOD] Testing new components

--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { DataContext } from "../../context/DataProvider";
 import styles from "./AboutSection.module.css";
-import { SkillCard } from "../../components/Sections";
+import { SkillCard, ExperienceCard } from "../../components/Sections";
 
 export function AboutSection() {
 
@@ -9,23 +9,26 @@ export function AboutSection() {
 
 
   const testSkillCard = data.About.TechStack.listData[0];
+  const testexperienceCard = data.About.Experience[0];
 
-  console.log("[ABOUT SECTION]", data.About, testSkillCard);
+  // console.log("[ABOUT SECTION]", data.About, testSkillCard, testexperienceCard);
 
   return (
     <>
       <section id="about" className={styles["about-sec"]}>
 
-        <SkillCard 
+        {/* <SkillCard 
           description={testSkillCard.skillDescription} 
           name={testSkillCard.skillName}
           tag={testSkillCard.tag}
           image={testSkillCard.image}
-        />
+        /> */}
 
         {/* <SkillCard 
           skillDataObj={testSkillCard}
         /> */}
+
+        <ExperienceCard experienceDataObj={testexperienceCard}/>
 
 
       </section>
