b5ec6089bf70577c38de41f0406c00fea6e16867
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-12 19:40:50 +0200

    [MOD] Re-Factoring Image Component

--- src/components/Images/Image/Image.tsx
+++ src/components/Images/Image/Image.tsx
@@ -1,38 +1,47 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, ReactElement } from 'react'
 
 import useCheckStaticFileExist from '../../../hooks/useCheckStaticFileExist'
 
+import { IMAGE_FITTING } from '../../../utils/enums'
+
 import styles from "./Image.module.css"
 
+
+interface CommonProps extends React.PropsWithChildren {
+    children?: ReactElement | ReactElement[],
+    id?: string,
+    className?: string,
+    style?: CSSProperties,
+}
 
 export interface ImageSize {
     xSize: string,
     ySize: string,
 }
 
-interface BaseProps extends React.PropsWithChildren {
-    id?: string,
-    className?: string,
-    style?: CSSProperties,
-    img?: string,
-    size?: ImageSize
+interface ImageProps extends CommonProps {
+    src?: string,
+    size?: ImageSize,
+    fittingMode?: IMAGE_FITTING,
 }
 
-interface ImageProps extends BaseProps {
-    data?: BaseProps;
+
+interface ComponentProps extends ImageProps {
+    data?: ImageProps;
 }
 
 export class ImagePropsBuilder {
 
     static build(
-        options: Partial<BaseProps> = {}
+        options: Partial<ComponentProps> = {}
     ) {
         return {
             id: options.id,
             className: options.className,
             style: options.style,
-            img: options.img,
+            src: options.src,
             size: options.size,
+            fittingMode: options.fittingMode,
         };
     }
 };
@@ -43,23 +52,25 @@ function getNoImage(): string {
 }
 
 
-const Image: React.FC<ImageProps> = ({
+const Image: React.FC<ComponentProps> = ({
     id = "",
     className = "",
     style = {},
-    img = "",
-    size,
+    src = "",
+    size = { xSize: "100%", ySize: "100%" },
+    fittingMode = IMAGE_FITTING.CONTAIN,
     data = {}
 }) => {
 
-    const { fileExists } = useCheckStaticFileExist( img );
+    const { fileExists } = useCheckStaticFileExist( src );
 
     const {
         id: dataId = id,
         className: dataClassName = className,
         style: dataStyle = style,
-        img: dataImg = img,
+        src: dataImg = src,
         size: dataSize = size,
+        fittingMode: dataFittingMode = fittingMode
     } = data;
 
     const combinedClassName = `${styles["image"]} ${dataClassName}`;
@@ -70,7 +81,7 @@ const Image: React.FC<ImageProps> = ({
         <img
             src={ fileExists ? dataImg : getNoImage() }
             id={dataId} className={combinedClassName}
-            style={{ ...dataStyle, width: xSize, height: ySize }}
+            style={{ ...dataStyle, width: xSize, height: ySize, objectFit: dataFittingMode }}
             >
         </img>
 
