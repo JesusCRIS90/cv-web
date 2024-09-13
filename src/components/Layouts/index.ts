e5ce2f543de947dba29c9954c2d009002ecf781c
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-14 00:15:20 +0200

    [MOD] Updated Layout index.ts file

--- src/components/Layouts/index.ts
+++ src/components/Layouts/index.ts
@@ -4,3 +4,7 @@ export { GridLayout1D, GridLayout1DPropsBuilder } from "./GridLayout1D/GridLayou
 export { GridLayout2D, GridLayout2DPropsBuilder } from "./GridLayout2D/GridLayout2D"
 export { ItemGridLayout, ItemGridLayoutPropsBuilder } from "./ItemGridLayout/ItemGridLayout"
 export { CenterLayout, CenterLayoutPropsBuilder } from "./CenterLayout/CenterLayout"
+export { CenteredBoxLayout, CenteredBoxLayoutBuilder } from "./CenteredBoxLayout/CenteredBoxLayout"
+export { Frame } from "./Frame/Frame"
+export { Separator } from "./Separator/Separator"
+export { ItemGridSpacer } from './ItemGridSpacer/ItemGridSpacer'
