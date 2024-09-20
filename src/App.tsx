854bd25a5a7ae5b0f62a29d7e528320747518c36
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-20 18:35:53 +0200

    [MOD] Updated App Component to work correctly with Customs Context Manager Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState, Dispatch, SetStateAction } from "react";
 
 import useLoadJSONFile, { RESULT_STATE } from "./hooks/useLoadJSONFile";
 
@@ -12,10 +12,22 @@ import { load_SVG_Sprites_Sheet } from "./utils/utilities";
 import { COMMON_ICONS_URL, SOCIAL_NETWORK_ICONS_URL, TECH_ICONS_URL } from "./utils/globalInfo";
 import { DataContext } from "./context/DataProvider";
 
+import { ContextStore } from './context/ContextManagerProvider'
+import { Manager } from "./context/managers";
+import useForceRender from "./hooks/useForceRender";
+
+
+export interface IReRenderAppComponent {
+  (): void;
+}
+
+export type AppStateDispatch = Dispatch<SetStateAction<RESULT_STATE>>;
 
 function App() {
-  
-  const { state, data } = useContext( DataContext );
+
+  const { appManager } = useContext(ContextStore);
+  const [state, setState] = useState<RESULT_STATE>(RESULT_STATE.LOADING);
+
 
   useEffect(() => {
     load_SVG_Sprites_Sheet(COMMON_ICONS_URL, "common-sprites-sheet");
@@ -23,11 +35,27 @@ function App() {
     load_SVG_Sprites_Sheet(TECH_ICONS_URL, "tech-sprites-sheet");
   }, []);
 
-  if (state === RESULT_STATE.LOADING) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
 
-  if (state === RESULT_STATE.ERROR) {
+    if (!appManager.hasContext("DISPATCHER-APP-STATE")) {
+
+      const dispatchAppState = new Manager<AppStateDispatch>(setState);
+      appManager.addContext<AppStateDispatch>("DISPATCHER-APP-STATE", dispatchAppState);
+
+      console.log("[APP]: Inserting Dispatcher app-state");
+    }
+
+
+  }, [])
+
+
+  console.log("APP")
+
+  // if (state === RESULT_STATE.LOADING) {
+  //   return <div>Loading...</div>;
+  // }
+
+  if (state === (RESULT_STATE.ERROR) || undefined) {
     return <div>Create a Error Page Here</div>;
   }
 
@@ -37,7 +65,6 @@ function App() {
       <AboutSection />
       <PortfolioSection />
       <NavBar />
-      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
