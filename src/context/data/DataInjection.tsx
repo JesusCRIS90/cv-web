a20fdce8dc0a6235e2f754f6af184b5b97216333
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-24 20:25:54 +0200

    [MOD] Updated DataInjection Component

--- src/context/data/DataInjection.tsx
+++ src/context/data/DataInjection.tsx
@@ -6,9 +6,19 @@ import useForceRender from '../../hooks/useForceRender';
 import { Manager } from '../managers';
 import { ContextStore } from '../ContextManagerProvider';
 
-import { WhoIamAdaptor, ContactAdaptor, ProfileImageAdaptor } from "../../adaptors"
-import { iWhoIAm, iContact, iProfileImage } from '../../interfaces';
+import { 
+    WhoIamAdaptor, 
+    ContactAdaptor, 
+    ProfileImageAdaptor,
+    BriefBioAdaptor,
+    ExperienceAdaptor,
+    SummaryAdaptor,
+    SkillsAdaptor,
+    PortfolioAdaptor
+} from "../../adaptors"
+import { iWhoIAm, iContact, iProfileImage, iBio, iSummaries, iExperiences, iSkills, iPortfolio } from '../../interfaces';
 import { AppStateDispatch } from '../../App';
+// import { BriefBioAdaptor, ExperienceAdaptor, SummaryAdaptor } from '../../adaptors/dataAdaptors';
 
 
 
@@ -28,8 +38,8 @@ const DataInjection = ({ children }: DataInjectionProps) => {
 
 
     useEffect(() => {
-        
-        if ( injected ) {
+
+        if (injected) {
             console.log("[DATA ALREADY INJECTED]")
             return;
         }
@@ -39,14 +49,18 @@ const DataInjection = ({ children }: DataInjectionProps) => {
             const dataState = new Manager<RESULT_STATE>(state);
             appManager.addContext<RESULT_STATE>("DATA-STATE", dataState);
             console.log("[Creating DATA-STATE]");
-        } 
+        }
 
         if (state === RESULT_STATE.SUCCESS && data !== null) {
-            
-            // Data Inyection
-            const { Home, Contact } = data;
-            const { WhoIam, ProfileImage } = Home;
 
+            // Data Inyection
+            const { Home, Contact, About, Portfolio } = data;
+            const { WhoIam, ProfileImage } = Home;
+            const { BriefBio, Experience, Skills, Summary } = About;
+
+            console.log("[DATA-INJECTION]", data);
+
+            // Home Section Data
             const whoIam = new Manager<iWhoIAm>(WhoIamAdaptor(WhoIam));
             appManager.addContext<iWhoIAm>("WHO-I-AM", whoIam);
 
@@ -56,32 +70,49 @@ const DataInjection = ({ children }: DataInjectionProps) => {
             const contact = new Manager<iContact>(ContactAdaptor(Contact));
             appManager.addContext<iContact>("CONTACT", contact);
 
+            // About Section Data
+            const bio  = new Manager<iBio>(BriefBioAdaptor(BriefBio));
+            appManager.addContext<iBio>("BRIEF-BIO", bio);
+
+            const summaries  = new Manager<iSummaries>(SummaryAdaptor(Summary));
+            appManager.addContext<iSummaries>("SUMMARY", summaries);
+
+            const experience  = new Manager<iExperiences>(ExperienceAdaptor(Experience));
+            appManager.addContext<iExperiences>("EXPERIENCE", experience);
+
+            const skills  = new Manager<iSkills>(SkillsAdaptor(Skills));
+            appManager.addContext<iSkills>("SKILLS", skills);
+
+            // Portfolio Section Data
+            const portfolio  = new Manager<iPortfolio>(PortfolioAdaptor(Portfolio));
+            appManager.addContext<iPortfolio>("PORTFOLIO", portfolio);
+
             // Updating RESULT STATE
             appManager.getContext<RESULT_STATE>("DATA-STATE")?.setData(state);
-            
+
             // Set Flag TRUE => Data Inyected
-            setInjected( true );
-        
-            console.log("DATA INJECTED CORRECTLY", injected);            
+            setInjected(true);
+
+            console.log("DATA INJECTED CORRECTLY", injected);
         }
 
     }, [state, injected]);
 
-    useEffect( () => {
+    useEffect(() => {
 
-        if ( injected ){
+        if (injected) {
             const dispactherAppState = appManager.getContext<AppStateDispatch>("DISPATCHER-APP-STATE")?.getData();
 
-            if ( dispactherAppState !== undefined ){
+            if (dispactherAppState !== undefined) {
                 // Triggering a Render on App Component, due to the fact that we are change its State (useState)
-                dispactherAppState( RESULT_STATE.SUCCESS );
+                dispactherAppState(RESULT_STATE.SUCCESS);
                 console.log("[DATA-INJECTED]: App Component Updated State");
             } else {
                 console.log("[DATA-INJECTED]: AppState Dispatcher Manager Empty")
-            }  
+            }
 
         }
-    }, [injected] );
+    }, [injected]);
 
 
 
