dc1986f3072143fd840f6136233c4a2644e6601d
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-27 01:32:25 +0200

    [MOD] Updated SubSections: Experience and Presentation

--- src/sections/AboutSection/SubSections/Experience/ExperienceSection.tsx
+++ src/sections/AboutSection/SubSections/Experience/ExperienceSection.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "./ExperienceSection.module.css";
 
-import { iExperiences } from "../../../../interfaces"
+import { iExperiences, iExperience } from "../../../../interfaces"
+import { Tittle } from "../../../../components";
+import { EXPERIENCE_CARD_MAX_WIDTH, EXPERIENCE_CARD_MIN_WIDTH } from "../../../../utils/globalInfo";
+import { Card, ResponsiveCardGrid } from "../../../../components/Layouts";
+import { ExperienceCard } from "../../../../components/Sections";
 
 export interface ExperiencePorps {
   ObjData: iExperiences | undefined,
@@ -15,12 +19,34 @@ const ExperienceSection: React.FC<ExperiencePorps> = ({
     return <></>
   }
 
-  console.log("[EXPERIENCE-SEC]",ObjData);
+  console.log("[EXPERIENCE-SEC]", ObjData);
 
   return (
     <>
       <section id="experience" className={styles["experience-sec"]}>
 
+        <Tittle>Experience</Tittle>
+
+        <ResponsiveCardGrid gap={10}>
+
+          {
+            ObjData.experience.map((experience: iExperience) => {
+              // ObjData.skills.map((skill: iSkill) => {
+
+              return (
+                <Card
+                  maxWidth={EXPERIENCE_CARD_MAX_WIDTH}
+                  minWidth={EXPERIENCE_CARD_MIN_WIDTH}
+                  key={experience.key}
+                >
+                  <ExperienceCard experience={experience} />
+                </Card>
+              )
+
+            })
+          }
+
+        </ResponsiveCardGrid>
 
       </section>
     </>
--- src/sections/AboutSection/SubSections/Presentation/PresentationSection.tsx
+++ src/sections/AboutSection/SubSections/Presentation/PresentationSection.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import styles from "./PresentationSection.module.css";
 
-import { iBio, iSummaries } from "../../../../interfaces"
+import { iBio, iParagraph, iSummaries, iSummary } from "../../../../interfaces"
+import { GridLayout1D, VerticalLayout } from "../../../../components/Layouts";
+import { SingleText, Tittle } from "../../../../components"
+import { SummaryCard } from "../../../../components/Sections";
+
 
 export interface PresentationsPorps {
   ObjBio: iBio | undefined,
@@ -17,12 +21,39 @@ const PresentationSection: React.FC<PresentationsPorps> = ({
     return <></>
   }
 
-  console.log("[PRESENTATION-SEC]",ObjBio, ObjSummary);
+  console.log("[PRESENTATION-SEC]", ObjBio, ObjSummary);
 
   return (
     <>
       <section id="presentation" className={styles["presentation-sec"]}>
 
+        <Tittle>ABOUT ME</Tittle>
+
+        <GridLayout1D distribution="50% auto">
+
+          <VerticalLayout>
+            {
+              ObjBio.bio.map((paragraph: iParagraph) => {
+                return (
+                  <SingleText key={paragraph.key}>
+                    {paragraph.text.text}
+                  </SingleText>
+                )
+              })
+            }
+          </VerticalLayout>
+
+          <GridLayout1D distribution="50% auto">
+            {
+              ObjSummary.summaries.map((summary: iSummary) => {
+                return (
+                  <SummaryCard summary={summary} key={summary.key}/>
+                )
+              })
+            }
+          </GridLayout1D>
+
+        </GridLayout1D>
 
       </section>
     </>
