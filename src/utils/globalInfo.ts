3d5b9816ad07fd0421b27d67be030c25c6e5e995
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2024-09-30 13:15:23 +0200

    [MOD] Updated GlobalInfo File

--- src/utils/globalInfo.ts
+++ src/utils/globalInfo.ts
@@ -30,7 +30,7 @@ export const sections: ISection[] = [
 
 
 
-export const PROJECT_CARD_MAX_WIDTH = 700;
+export const PROJECT_CARD_MAX_WIDTH = 650;
 export const PROJECT_CARD_MIN_WIDTH = PROJECT_CARD_MAX_WIDTH * 0.8;
 
 export const SKILL_CARD_MAX_WIDTH = 400;
